2b2b4482fd77a8717947e5b9d83ef664dc2915e0
Gerson Samuel Fuchs <fuchsgerson@gmail.com>
2023-05-09 14:23:41 -0300

    feat(app/(parentheses))-login and logout escape directory structure

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Page: FC = () => {
+  return <div>login</div>;
+};
+
+export default Page;
--- src/pages/dashboard.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-
-const Dashboard: FC = () => {
-  return <div>Dashboard - This still works</div>;
-};
-export default Dashboard;
